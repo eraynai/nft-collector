feb1aa9e39d26068072344022add5ba554af5336
Elli Raynai <elli@elliraynai.com>
2021-07-07 17:39:23 -0400

    second model is working

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from . models import Nft
+from . models import Bids, Nft
 
 # Register your models here.
 admin.site.register(Nft)
+admin.site.register(Bids)
--- main_app/models.py
+++ main_app/models.py
@@ -8,3 +8,10 @@ class Nft(models.Model):
     description = models.CharField(max_length=250)
     price = models.IntegerField()
     category = models.CharField(max_length=100)
+
+
+class Bids(models.Model):
+    date = models.DateField()
+    bid = models.FloatField()
+    user = models.CharField(max_length=250)
+    nft = models.ForeignKey(Nft, on_delete=models.CASCADE)
