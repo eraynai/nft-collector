e1e7d4b0eaa200fb3ab4eb1eadc90b39d8df2d07
Elli Raynai <elli@elliraynai.com>
2021-07-08 15:30:20 -0400

    Revert "added the bid model one to many model to the detail template"
    This reverts commit addf9918b27edceed683ceb75a59e983b845682e.

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from . models import Bids, Nft, Photos
+from . models import Bids, Nft
 
 # Register your models here.
 admin.site.register(Nft)
 admin.site.register(Bids)
-admin.site.register(Photos)
